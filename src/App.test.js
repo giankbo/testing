6ca13a826b366d8185ed3ebb7daea8ee8b00a743
Gian <e.dolphinxyz@gmail.com>
2022-08-31 21:41:02 -0500

    Add todo code

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import * as ReactDOM from 'react-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('ToDo', () => {
+  const root = document.createElement('div');
+  ReactDOM.render(<App />, root);
+
+  // after rendering our component
+  // use DOM APIs (query selector) to make assertions
+  expect(root.querySelector('h1').textContent).toBe('TODO');
+  expect(root.querySelector('label').textContent).toBe('Add todo: ');
+  expect(root.querySelector('button').textContent).toBe('Add #1');
 });
